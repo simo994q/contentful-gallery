bcb4e441c30c11602fa1e1721c487a26c6ab37b7
simo994q <simonkn1@hotmail.com>
2023-09-28 09:48:43 +0200

    start fetch

--- contentful-gallery/src/App.jsx
+++ contentful-gallery/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer' // skal kun bruges til rich text formater
+import * as contentful from 'contentful'
 
 function App() {
+
+  const [data, setData] = useState()
+
+  const client = contentful.createClient({
+    space: `${import.meta.env.VITE_PUBLIC_SPACE_ID}`,
+    environment: 'master',
+    accessToken: `${import.meta.env.VITE_PUBLIC_ACCESS_TOKEN}`
+  })
+
+  useEffect(() => {
+    client.getEntries()
+    .then((entry) => setData(entry))
+    .catch(console.error)
+  }, [])
+
   return (
     <>
-    
+
     </>
   )
 }
